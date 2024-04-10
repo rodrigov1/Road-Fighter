60f8b43fb25274138c8f6bfe95f6b7e4f6a60a97
Simon Saillen <simonsaillen29@gmail.com>
2024-04-10 15:25:20 -0300

    feat: main menu, better texts and other improvements

--- src/game.py
+++ src/game.py
@@ -9,7 +9,7 @@ car_width = 56
 car_height = 100
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('A bit Racey')
+pygame.display.set_caption('Road-Fighter')
 
 carImage = pygame.image.load('images/car.png')
 bgImage = pygame.image.load("images/road.png")
@@ -106,17 +106,18 @@ def game_loop():
 
     return False
 
-def message_display(text):
+def message_display(text, shift, color):
     large_text = pygame.font.Font('freesansbold.ttf', 50)
-    text_surface = large_text.render(text, True, (0, 0, 0))
+    text_surface = large_text.render(text, True, color)
     text_rect = text_surface.get_rect()
-    text_rect.center = (display_width // 2, display_height // 2)
+    text_rect.center = (display_width / 2, (display_height / 2) - shift)
     gameDisplay.blit(text_surface, text_rect)
     pygame.display.update()
 
 def game_over():
     gameDisplay.fill((255, 255, 255))
-    message_display('Game Over! Press R to restart or Q to quit.')
+    message_display("GAME OVER!",45, "RED")
+    message_display("Press R to restart or Q to quit.",0, "BLACK")
 
     while True:
         for event in pygame.event.get():
@@ -130,7 +131,26 @@ def game_over():
                 elif event.key == pygame.K_r:
                     return
 
+def start_screen(): 
+    gameDisplay.fill((255, 255, 255))
+    message_display("ROAD FIGHTER", 120, "BLACK")
+    message_display("Press P to play", 0, "GREEN")
+    message_display("Press Q to quit", -60, "RED")
+    
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+            pygame.quit()
+            quit()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+            return
+        else:
+            continue
+
+
 # Loop principal del juego
+start_screen()
+
 while True:
     if game_loop():
         game_over()
