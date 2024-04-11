daaa00fa2f5a9074f2b79b04ba88dd2549932e51
Simon Saillen <simonsaillen29@gmail.com>
2024-04-11 10:45:06 -0300

    fix: fixes on car distribution, new images for enemy cars and player car, minor code fixes

--- src/game.py
+++ src/game.py
@@ -11,16 +11,17 @@ car_height = 100
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Road-Fighter')
 
-carImage = pygame.image.load('images/car.png')
+enemyImage = pygame.image.load('images/car_enemy.png')
+playerImage = pygame.image.load('images/car_player.png')
 bgImage = pygame.image.load("images/road.png")
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = carImage
+        self.image = playerImage
         self.rect = self.image.get_rect()
-        self.rect.x = (display_width * 0.45)
-        self.rect.y = (display_height * 0.8)
+        self.rect.x = (int(display_width * 0.45))
+        self.rect.y = (int(display_height * 0.8))
         self.x_change = 0
         self.y_change = 0
 
@@ -28,8 +29,8 @@ class Player(pygame.sprite.Sprite):
         self.rect.x += self.x_change
         self.rect.y += self.y_change
 
-        if self.rect.x <= 234.5:
-            self.rect.x = 234.5
+        if self.rect.x <= 234:
+            self.rect.x = 234
         elif self.rect.x >= 556 - car_width:
             self.rect.x = 556 - car_width
 
@@ -41,9 +42,9 @@ class Player(pygame.sprite.Sprite):
 class OpponentCar(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = carImage
+        self.image = enemyImage
         self.rect = self.image.get_rect()
-        self.rect.x = random.choice((300, 400, 500))
+        self.rect.x = random.choice((248,332,420,500))
         self.rect.y = -100
         self.speed = random.choice((3, 4, 5, 6, 7, 8))
 
@@ -56,7 +57,7 @@ def game_loop():
     all_sprites.add(player)
 
     opponents = pygame.sprite.Group()
-    for _ in range(4):
+    for _ in range(5):
         opponent = OpponentCar()
         all_sprites.add(opponent)
         opponents.add(opponent)
@@ -92,7 +93,7 @@ def game_loop():
         # Generar nuevos obstáculos cuando alguno deje la pantalla
         for opponent in opponents:
             if opponent.rect.y > display_height:
-                opponent.rect.x = random.choice((300, 400, 500))
+                opponent.rect.x = random.choice((248,332,420,500))
                 opponent.rect.y = -100
                 opponent.speed = random.choice((3, 4, 5, 6, 7, 8))
 
@@ -110,7 +111,7 @@ def message_display(text, shift, color):
     large_text = pygame.font.Font('freesansbold.ttf', 50)
     text_surface = large_text.render(text, True, color)
     text_rect = text_surface.get_rect()
-    text_rect.center = (display_width / 2, (display_height / 2) - shift)
+    text_rect.center = (int(display_width / 2), int((display_height / 2) - shift))
     gameDisplay.blit(text_surface, text_rect)
     pygame.display.update()
 
