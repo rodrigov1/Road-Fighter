e4ad4a704fca6d828a061ce2b064dc0cc21fff3d
KevinGTH <kevin.bejarano@mi.unc.edu.ar>
2024-05-30 17:06:09 -0300

    fix:correcion de test_player

--- src/tests/test_player.py
+++ src/tests/test_player.py
@@ -17,31 +17,31 @@ def test_player_initialization(player):
     assert player.posX == 250
     assert player.posY == 300
     assert player.speed == 10
-    assert player.rect.center == [250, 300]
+    assert player.rect.center == (250, 300)
 
 def test_player_move_right_within_bounds(player):
     player.update("right")
     assert player.posX == 260
-    assert player.rect.center == [260, 300]
+    assert player.rect.center == (260, 300)
 
 #Compruebo si la posicion no cambia cuando estoy en el borde derecho
 def test_player_move_right_outside_bounds(player):
     player.posX = ROAD_RIGHT_BORDER
     player.update("right")
     assert player.posX == ROAD_RIGHT_BORDER
-    assert player.rect.center == [ROAD_RIGHT_BORDER, 300]
+    assert player.rect.center == (ROAD_RIGHT_BORDER, 300)
 
 def test_player_move_left_within_bounds(player):
     player.update("left")
     assert player.posX == 240
-    assert player.rect.center == [240, 300]
+    assert player.rect.center == (240, 300)
 
 #Compruebo si la posicion no cambia cuando estoy en el borde izquierdo
 def test_player_move_left_outside_bounds(player):
     player.posX = ROAD_LEFT_BORDER
     player.update("left")
     assert player.posX == ROAD_LEFT_BORDER
-    assert player.rect.center == [ROAD_LEFT_BORDER, 300]
+    assert player.rect.center == (ROAD_LEFT_BORDER, 300)
 
 def test_player_update_method_calls(player):
     #Mockeo el metodo update para contar cuantas veces se llama
