5518aa30dbb1726fb4db47e03ecda3d56ecc7c19
rodrigov1 <rodrigo.vargas@mi.unc.edu.ar>
2024-06-14 18:32:43 -0300

    feat: observer pattern implemented

---
+++ src/observerp.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+class Subscriber(ABC):
+    @abstractmethod
+    def updateSub(self):
+        raise NotImplementedError("This method should be overridden by subclasses")
+
+
+class Publisher:
+    def __init__(self):
+        self.listeners = []
+
+    def subscribe(self, listener):
+        if isinstance(listener, Subscriber):
+            self.listeners.append(listener)
+        else:
+            raise TypeError("Listener must be an instance of Subscriber")
+
+    def notify(self):
+        for listener in self.listeners:
+            listener.updateSub()
