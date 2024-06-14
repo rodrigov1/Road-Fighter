0edaa31930d4f1ff62e6fe9a6c5169837263516d
rodrigov1 <rodrigo.vargas@mi.unc.edu.ar>
2024-06-14 18:33:08 -0300

    fix: powerup updated

--- src/powerup.py
+++ src/powerup.py
@@ -1,10 +1,19 @@
 import pygame
+import random
+from screen import ROAD_LEFT_BORDER, ROAD_RIGHT_BORDER
 
 
 class PowerUp(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self):
         super().__init__()
         self.image = pygame.image.load("images/rainbow_car.png").convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.posX = random.randint(ROAD_LEFT_BORDER, ROAD_RIGHT_BORDER)
+        self.posY = random.randint(0, 800) * -1
+        self.rect.x = self.posX
+        self.rect.y = self.posY
+        self.speed = 5
+
+    def update(self):
+        self.posY += self.speed
+        self.rect.center = [self.posX, self.posY]
