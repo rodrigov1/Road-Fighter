465e60dad09445263df5d705ff3cbb1ba065b73e
rodrigov1 <rodrigo.vargas@mi.unc.edu.ar>
2024-06-17 15:08:34 -0300

    fix: player methods updated and improved

--- src/player.py
+++ src/player.py
@@ -1,11 +1,12 @@
 import pygame
 from screen import ROAD_LEFT_BORDER, ROAD_RIGHT_BORDER
+from observerp import Subscriber
 
 
-class Player(pygame.sprite.Sprite):
+class Player(Subscriber, pygame.sprite.Sprite):
     def __init__(self, posX, posY, speed):
         super().__init__()
-        self.image = pygame.image.load("images/red_car.png").convert_alpha()
+        self.image = pygame.image.load("../images/red_car.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.posX = posX
         self.posY = posY
@@ -26,49 +27,41 @@ class Player(pygame.sprite.Sprite):
 
         self.rect.center = (self.posX, self.posY)
 
-    def check_PowerUp(self, powerup):
-        # Check if player collided with powerup, if so, return True
-        hit = pygame.sprite.spritecollide(self, powerup, True)
-
-        return len(hit) > 0
-
-    def health(self, health):
+    def updateHealth(self, health):
         if self.color == "Red":
             match health:
                 case 3:
-                    self.image = pygame.image.load("images/red_car.png").convert_alpha()
+                    self.image = pygame.image.load(
+                        "../images/red_car.png"
+                    ).convert_alpha()
                 case 2:
                     self.image = pygame.image.load(
-                        "images/red_car_1.png"
+                        "../images/red_car_1.png"
                     ).convert_alpha()
                 case 1:
                     self.image = pygame.image.load(
-                        "images/red_car_2.png"
+                        "../images/red_car_2.png"
                     ).convert_alpha()
         else:
             match health:
                 case 3:
                     self.image = pygame.image.load(
-                        "images/dark_blue_car.png"
+                        "../images/dark_blue_car.png"
                     ).convert_alpha()
                 case 2:
                     self.image = pygame.image.load(
-                        "images/dark_blue_car_1.png"
+                        "../images/dark_blue_car_1.png"
                     ).convert_alpha()
                 case 1:
                     self.image = pygame.image.load(
-                        "images/dark_blue_car_2.png"
+                        "../images/dark_blue_car_2.png"
                     ).convert_alpha()
 
-    def change_car(self, color):
-        match color:
-            case "Blue":
-                self.image = pygame.image.load(
-                    "images/dark_blue_car.png"
-                ).convert_alpha()
+    def updateSub(self, powerup):
+        match powerup:
+            case "Freeze":
                 self.color = "Blue"
-            case "Pink":
-                self.image = pygame.image.load("images/pink_car.png").convert_alpha()
+            case "Heat":
+                self.image = pygame.image.load("../images/pink_car.png").convert_alpha()
             case "Reset":
-                self.image = pygame.image.load("images/red_car.png").convert_alpha()
                 self.color = "Red"
