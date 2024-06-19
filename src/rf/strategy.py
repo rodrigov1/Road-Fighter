0017545d4612eb3321d633b7728dc37e903d69ee
rodrigov1 <rodrigo.vargas@mi.unc.edu.ar>
2024-06-19 18:28:52 -0300

    fix: cleaning code

--- src/rf/strategy.py
+++ src/rf/strategy.py
@@ -9,14 +9,6 @@ class MovementStrategy:
         raise NotImplementedError("This method should be overridden by subclasses")
 
 
-class RandomMovement(MovementStrategy):
-    def move(self, enemy):
-        enemy.rect.y += enemy.speed
-        # Check if the enemy is within the road boundaries for random movement
-        if ROAD_LEFT_BORDER <= enemy.rect.x <= ROAD_RIGHT_BORDER:
-            enemy.rect.x += random.randint(-enemy.speed, enemy.speed)
-
-
 class StillMovement(MovementStrategy):
     def move(self, enemy):
         enemy.posY += enemy.speed
